0900b627c446505b433548ee90ac4f393745a2c9
SudhapavaniCh <sudhapavanichowdary95@gmail.com>
2020-11-04 15:29:43 -0600

    add weekend and weekday files

--- app.js
+++ app.js
@@ -8,20 +8,19 @@ const app= express();
 
 app.get('/',function(req,res){
 
-  res.setHeader('Content-Type', 'text/html');
-
 var today= new Date();
 
 if(today.getDay()===6||today.getDay()===0){
 
-  res.write("<h1>Its the weekend yahhh</h1>");
-}else{
-  res.write('<h2>Its the weekday</h2>');
+  res.sendFile(__dirname + "/weekend.html");
+} else{
+
+  res.sendFile(__dirname + "/weekday.html");
 }
-res.write("\n Today is : " + today);
-res.send();
 
 })
+
+
 //create server
 app.listen('3000',function(){
   console.log('Server is listening at port 3000');
