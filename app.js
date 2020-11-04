a11c0fc2a18fa89ca51b34e0d61b7614dafc1ec0
SudhapavaniCh <sudhapavanichowdary95@gmail.com>
2020-11-04 16:34:38 -0600

    Addition of the whole week days

--- app.js
+++ app.js
@@ -12,12 +12,34 @@ app.get('/',function(req,res){
 var today= new Date();
 var currentDay=today.getDay();
 var day=" ";
-if(currentDay===6||currentDay===0){
-day= "Weekend";
-} else{
-day="Weekday";
+switch (currentDay) {
+    case 0:
+      day="Sunday";
+      break;
+    case 1:
+      day="Monday";
+      break;
+    case 2:
+      day="Tuesday";
+      break;
+    case 3:
+      day="Wednesday";
+      break;
+    case 4:
+      day="Thursday";
+      break;
+    case 5:
+      day="Friday";
+      break;
+    case 6:
+      day="Saturday";
+      break;
+    default:
+      day="Not a day";
+
 }
-res.render("lists", {dayToday:day});
+
+res.render("lists", {dayToday:day, completeDay:today});
 });
 
 
