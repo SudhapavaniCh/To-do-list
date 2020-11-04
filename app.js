e3f428fb008c0f69cdce3477175128eeeb43ffe3
SudhapavaniCh <sudhapavanichowdary95@gmail.com>
2020-11-04 16:25:15 -0600

    Ejs update

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ const bodyParser= require('body-parser');
 //set up express app
 const app= express();
 
+app.set('view engine', 'ejs');
+
 app.get('/',function(req,res){
-
 var today= new Date();
-
-if(today.getDay()===6||today.getDay()===0){
-
-  res.sendFile(__dirname + "/weekend.html");
+var currentDay=today.getDay();
+var day=" ";
+if(currentDay===6||currentDay===0){
+day= "Weekend";
 } else{
-
-  res.sendFile(__dirname + "/weekday.html");
+day="Weekday";
 }
-
-})
+res.render("lists", {dayToday:day});
+});
 
 
 //create server
 app.listen('3000',function(){
   console.log('Server is listening at port 3000');
-})
+});
