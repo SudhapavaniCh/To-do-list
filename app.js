09c9b3090e6f737a6139d53fe06242099cfd04b5
SudhapavaniCh <sudhapavanichowdary95@gmail.com>
2020-11-04 19:11:48 -0600

    A to do list in working condition

--- app.js
+++ app.js
@@ -1,44 +1,34 @@
 //jshint esversion:6
-
 const express= require('express');
 const bodyParser= require('body-parser');
 
 //set up express app
 const app= express();
+let items = ["yoga", "gym", "cooking"];
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended:true}));
+//get request
 app.get('/',function(req,res){
-var today= new Date();
-var currentDay=today.getDay();
-var day=" ";
-switch (currentDay) {
-    case 0:
-      day="Sunday";
-      break;
-    case 1:
-      day="Monday";
-      break;
-    case 2:
-      day="Tuesday";
-      break;
-    case 3:
-      day="Wednesday";
-      break;
-    case 4:
-      day="Thursday";
-      break;
-    case 5:
-      day="Friday";
-      break;
-    case 6:
-      day="Saturday";
-      break;
-    default:
-      day="Not a day";
-}
-res.render("lists", {dayToday:day});
+ let options={
+   weekday:'short',
+   month:'short',
+   day:'numeric',
+   year:'numeric'
+ }
+let today= new Date();
+let day=today.toLocaleDateString("en-US",options);
+res.render("lists", {dayToday: day, newListItems: items});
 });
+
+//post request
+app.post('/',function(req,res){
+let item= req.body.newitem;
+items.push(item);
+   res.redirect('/');
+});
+
 //completeDay:today, this is the json object to sent back todays date with day and time as well
 //create server
 app.listen('3000',function(){
